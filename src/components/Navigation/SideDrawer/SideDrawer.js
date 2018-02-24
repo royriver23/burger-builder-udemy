fbf8f906e652363505a7be0f85e5da3e658618e2
Roy Rivera <roy.fita23@gmail.com>
2018-02-23 23:16:44 -0600

    Final SideDrawer and Toolbar work

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Aux';
+
+const sideDrawer = (props) => {
+  const attachedClasses = [classes.SideDrawer, (props.open ? classes.Open : classes.Close)];
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+}
+
+export default sideDrawer;
